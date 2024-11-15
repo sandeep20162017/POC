487b281eb413427abd5d6f47ec4dc723256e55fa
San Kanao <sandeep.kanao@gmail.com>
2024-11-15 18:46:04 -0500

    Add files via upload

--- Reports/Startup.cs
+++ Reports/Startup.cs
@@ -1,25 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Telerik.Reporting.Cache.File;
 using Telerik.Reporting.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Register Telerik Reporting services
-builder.Services.AddControllersWithViews();
-builder.Services.AddSingleton<IReportSourceResolver>(sp =>
-    new ReportFileResolver(System.IO.Path.Combine(builder.Environment.ContentRootPath, "Reports"))
-        .AddFallbackResolver());
-builder.Services.AddSingleton<IReportServiceConfiguration>(sp => new ReportServiceConfiguration
+namespace BCES
 {
-    Storage = new FileStorage(),
-    ReportSourceResolver = sp.GetRequiredService<IReportSourceResolver>(),
-    HostAppId = "MyApp",
-    ReportSharingTimeout = 60,
-    MaxConcurrentReportExecutions = 2
-});
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-var app = builder.Build();
+        public IConfiguration Configuration { get; }
 
-app.UseStaticFiles();
-app.UseRouting();
-app.MapControllers();
-app.Run();
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add support for controllers with views
+            services.AddControllersWithViews();
+
+            // Configure Telerik Reporting services
+            services.AddSingleton<IReportServiceConfiguration>(sp => new ReportServiceConfiguration
+            {
+                Storage = new FileStorage(),
+                HostAppId = "BCESApp",
+                ReportSharingTimeout = 60,
+                MaxConcurrentReportExecutions = 2
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
