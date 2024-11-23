001d0c31a2984b0d08ec7c31f37c3d7ac8413c5e
San Kanao <sandeep.kanao@gmail.com>
2024-11-23 17:18:00 -0500

    Add files via upload

--- Sites/AspExpert/5 Controller.cs
+++ Sites/AspExpert/5 Controller.cs
@@ -1,35 +1,31 @@
 [HttpGet]
 public IActionResult GetUsers()
 {
+    // Fetch pre-aggregated data with schema SCES
     var users = db.Query<UserViewModel>(
         @"SELECT u.UserId, u.UserName, u.RoleId, r.RoleName,
-            (SELECT SiteId FROM UserSite WHERE UserId = u.UserId) AS SiteIds
-          FROM User u
-          INNER JOIN Role r ON u.RoleId = r.RoleId").ToList();
+                 STRING_AGG(CAST(us.SiteId AS VARCHAR), ',') AS SiteIds
+          FROM SCES.[User] u
+          INNER JOIN SCES.[Role] r ON u.RoleId = r.RoleId
+          LEFT JOIN SCES.UserSite us ON u.UserId = us.UserId
+          GROUP BY u.UserId, u.UserName, u.RoleId, r.RoleName").ToList();
 
-    var roles = db.Query<RoleModel>("SELECT RoleId, RoleName FROM Role").ToList();
-    var sites = db.Query<SiteModel>("SELECT SiteId, SiteName FROM Site").ToList();
+    // Convert SiteIds from comma-separated string to List<int>
+    foreach (var user in users)
+    {
+        user.SiteIds = string.IsNullOrEmpty(user.SiteIdsString)
+            ? new List<int>()
+            : user.SiteIdsString.Split(',').Select(int.Parse).ToList();
+    }
 
+    // Fetch roles and sites for dropdowns
+    var roles = db.Query<RoleModel>("SELECT RoleId, RoleName FROM SCES.[Role]").ToList();
+    var sites = db.Query<SiteModel>("SELECT SiteId, SiteName FROM SCES.Site").ToList();
+
+    // Pass dropdown data to the view
     ViewData["Roles"] = roles;
     ViewData["Sites"] = sites;
 
+    // Return JSON result for the grid
     return Json(users);
-}
-
-[HttpPost]
-public IActionResult UpdateUser([FromBody] UserViewModel model)
-{
-    // Update User table
-    db.Execute("UPDATE User SET UserName = @UserName, RoleId = @RoleId WHERE UserId = @UserId",
-        new { model.UserName, model.RoleId, model.UserId });
-
-    // Update UserSite table
-    db.Execute("DELETE FROM UserSite WHERE UserId = @UserId", new { model.UserId }); // Clear existing entries
-    foreach (var siteId in model.SiteIds)
-    {
-        db.Execute("INSERT INTO UserSite (UserId, SiteId) VALUES (@UserId, @SiteId)",
-            new { model.UserId, SiteId = siteId });
-    }
-
-    return Json(new { success = true });
 }
