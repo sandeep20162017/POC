97a2f8e1c87238e8bcd289ef44e293356442552a
San Kanao <sandeep.kanao@gmail.com>
2025-02-03 18:52:29 -0500

    Create MakevsBuyModel.cs

---
+++ MakevsBuy/MakevsBuyModel.cs
@@ -0,0 +1,28 @@
+namespace YourProject.Models
+{
+    public class MbList
+    {
+        public string? MbNumber { get; set; }
+        public string? Keyword { get; set; }
+        public string? Description { get; set; }
+        public string? BusType { get; set; }
+        public string? NumBusComp { get; set; }
+        public string? VendorPartNum { get; set; }
+        public string? BuyCode { get; set; }
+        public string? RebuildCode { get; set; }
+        public string? CoreCode { get; set; }
+        public string? AnnualUsage { get; set; }
+        public string? BenchNumber { get; set; }
+        public string? SampleProvided { get; set; }
+        public string? Recommendation { get; set; }
+        public string? RemanCost { get; set; }
+        public string? ExternalCost { get; set; }
+        public string? BuyNewCost { get; set; }
+        public string? InternalCost { get; set; }
+        public string? SopNumber { get; set; }
+        public string? CoreCost { get; set; }
+        public string? JobNumber { get; set; }
+        public decimal ManualPrice { get; set; }
+        public int RowId { get; set; }
+    }
+}
